9a61b970d205291a0aa5a1132461bb231ef6d51b
Reaper <victorburov20@gmail.com>
2024-10-08 19:39:07 +0300

    Создание меню(1)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,6 +19,27 @@ namespace KR
         public MainWindow()
         {
             InitializeComponent();
+            Main_menu.Children.Add(Set_Image("Images/Main_Bg_lvl1.png",1920,1080));
+            Main_menu.Children.Add(Set_Image("Images/hills_lvl1.png",1920,1080));
+            //Main_menu.Children.Add(Set_Image("C:\\Users\\ПК\\Desktop\\Kyrs\\KR\\Images\\Light_lvl2.png", 1920, 1080));
+        }
+        public Image Set_Image(string image_name,double Widht,double Height)
+        {
+            Image image = new Image();
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(image_name, UriKind.RelativeOrAbsolute);
+            bitmap.EndInit();
+            image.Source = bitmap;
+            image.Width =Widht;
+            image.Height =Height;
+            return image;
+        }
+        public Button Set_Button(string content)
+        {
+            Button button = new Button();
+            button.Content = content;
+            return button;
         }
     }
 }
