e82acae3ef92978fcb6b0e8cfc8db38682543c69
Reaper <victorburov20@gmail.com>
2024-10-16 13:15:51 +0300

    Начало работы над Редактором

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Xml.Linq;
 
 namespace KR
 {
@@ -22,81 +23,8 @@ namespace KR
         public MainWindow()
         {
             InitializeComponent();
-            ResourceDictionary resourceDictionary = new ResourceDictionary();
-            resourceDictionary.Source = new Uri("Styles/Main_menu.xaml",UriKind.RelativeOrAbsolute);
-            this.Resources.MergedDictionaries.Add(resourceDictionary);
-            Main_menu_can.Width = 1920;
-            Main_menu_can.Height = 1080;
-            Main_menu_can.Children.Add(Set_Image("Images/Main_Bg_lvl1.png",1920,1080));
-            List<Image> clouds = new List<Image>();
-            for (int i = 1; i < 4; i++)
-            {
-                clouds.Add(Set_Image("Images/cloud_" + Convert.ToString(i) + "_lvl1.png", 800, 250));
-                Canvas.SetLeft(clouds[i - 1], -800*i);
-                Canvas.SetTop(clouds[i - 1], new Random().Next(-200, 512));
-                Main_menu_can.Children.Add(clouds[i - 1]);
-                MoveImage(clouds[i - 1],-800*i, 1920, (800*i + 1920)/54.4);          
-            }
-            Main_menu_can.Children.Add(Set_Image("Images/hills_lvl1.png",1920,1080));
-            Button button;
-            button = Set_Button("Play", (Style)this.Resources["lvl1"]);
-            Canvas.SetLeft(button,(Main_menu_can.Width - button.Width) / 2);
-            Canvas.SetTop(button,(Main_menu_can.Height - button.Height) / 2);
-            Main_menu_can.Children.Add(button);
-            button = Set_Button("Redactor", (Style)this.Resources["lvl1"]);
-            Canvas.SetLeft(button, (Main_menu_can.Width - button.Width) / 2);
-            Canvas.SetTop(button, (Main_menu_can.Height - button.Height*4) / 2);
-            Main_menu_can.Children.Add(button);
-            button = Set_Button("Exit", (Style)this.Resources["lvl1"]);
-            Canvas.SetLeft(button, (Main_menu_can.Width - button.Width) / 2);
-            Canvas.SetTop(button, (Main_menu_can.Height + button.Height * 2) / 2);
-            Main_menu_can.Children.Add(button);
-            DispatcherTimer _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromMilliseconds(50);
-            //_timer.Tick += MoveButton;
-            _timer.Start();
-            //Main_menu.Children.Add(Set_Image("C:\\Users\\ПК\\Desktop\\Kyrs\\KR\\Images\\Light_lvl2.png", 1920, 1080));
-        }
-        public Image Set_Image(string image_name,double Widht,double Height)
-        {
-            Image image = new Image();
-            image.Width = Widht;
-            image.Height = Height;
-            BitmapImage bitmap = new BitmapImage();
-            bitmap.BeginInit();
-            bitmap.UriSource = new Uri(image_name, UriKind.RelativeOrAbsolute);
-            bitmap.EndInit();
-            image.Source = bitmap;
-            return image;
-        }
-        public Button Set_Button(string content,Style st)
-        {
-            Button button = new Button();
-            button.Content = content;
-            button.Name = content;
-            button.Style = st;
-            return button;
-        }
-        private void MoveImage(Image image,double fromX,double toX,double duraction)
-        {
-            DoubleAnimation moveXAnimation = new DoubleAnimation()
-            {
-                From = fromX,
-                To = toX,
-                Duration = TimeSpan.FromSeconds(duraction),
-                FillBehavior = FillBehavior.HoldEnd,
-            };
-            Storyboard storyboard = new Storyboard();
-            storyboard.Children.Add(moveXAnimation);
-            Storyboard.SetTarget(moveXAnimation,image);
-            Storyboard.SetTargetProperty(moveXAnimation, new PropertyPath("(Canvas.Left)"));
-            moveXAnimation.Completed += (s, e) =>
-            {
-                Canvas.SetLeft(image, 0);
-                Canvas.SetTop(image, new Random().Next(-200, 512));
-                storyboard.Begin();
-            };
-            storyboard.Begin();
+            Main_Frame.Content = new Main_menu_page();
         }
     }
 }
+
---
+++ Redactor_page.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KR
+{
+    /// <summary>
+    /// Логика взаимодействия для Redactor_page.xaml
+    /// </summary>
+    public partial class Redactor_page : Page
+    {
+        public Redactor_page()
+        {
+            InitializeComponent();
+            List<Button> Titles = new List<Button>();
+            Titles.Add(Set_Button("pack://application:,,,/Images/Tiles/Tile_Road_lvl1.png", 80, 80, null));
+            for(int i = 1; i < 2; i++)
+            {
+                Titles.Add(Set_Button("pack://application:,,,/Images/Tiles/Tile_grass_" + Convert.ToString(i) + "_lvl1.png", 80, 80, (Style)this.Resources["Titles"]));
+            }
+            for(int i = 0; i < Titles.Count(); i++)
+            {
+                Redactor_Stack_pan.Children.Add(Titles[i]);
+            }
+        }
+        public Button Set_Button(string content, Style st)
+        {
+            Button button = new Button();
+            button.Content = content;
+            button.Name = content;
+            button.Style = st;
+            return button;
+        }
+        public Button Set_Button(string way,float width,float height,Style st)
+        {
+            Button button = new Button();
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(way, UriKind.RelativeOrAbsolute);
+            bitmap.EndInit();
+            ImageBrush imageBrush = new ImageBrush(bitmap);
+            button.Background = imageBrush;
+            button.Style = st;
+            return button;
+        }
+    }
+}
