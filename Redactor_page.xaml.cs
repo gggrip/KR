86fb5a871ee7045a7c47d6507ddd5f38bd6bc2c3
Reaper <victorburov20@gmail.com>
2024-10-19 00:10:29 +0300

    Редактор - создание сетки кнопок + новые тайлы

--- Redactor_page.xaml.cs
+++ Redactor_page.xaml.cs
@@ -20,6 +20,7 @@ namespace KR
     /// </summary>
     public partial class Redactor_page : Page
     {
+        Button button_SHAB = new Button();
         public Redactor_page()
         {
             InitializeComponent();
@@ -27,13 +28,15 @@ namespace KR
             resourceDictionary.Source = new Uri("Styles/Main_menu.xaml", UriKind.RelativeOrAbsolute);
             this.Resources.MergedDictionaries.Add(resourceDictionary);
             List<Button> Titles = new List<Button>();
-            for (int i = 1; i < 6; i++)
+            for (int i = 1; i < 14; i++)
             {
                 Titles.Add(Set_Button("pack://application:,,,/Images/Tiles/Tile_road_" +Convert.ToString(i)+ "_lvl1.png", 80, 80, (Style)this.Resources["Tiles"]));
+                Titles[Titles.Count() - 1].Click += Button_vib_Click;
             }
             for (int i = 1; i < 5; i++)
             {
                 Titles.Add(Set_Button("pack://application:,,,/Images/Tiles/Tile_grass_" + Convert.ToString(i) + "_lvl1.png", 80, 80, (Style)this.Resources["Tiles"]));
+                Titles[Titles.Count() - 1].Click += Button_vib_Click;
             }
             for(int i = 0; i < Titles.Count(); i++)
             {
@@ -45,12 +48,29 @@ namespace KR
                 Grid.SetRow(Titles[i], i/2);
                 Grid.SetColumn(Titles[i], i%2); 
             }
+            for (int j = 0; j < 24; j++)
+            {
+                Redactor_map.ColumnDefinitions.Add(new ColumnDefinition());
+                Redactor_map.ColumnDefinitions[j].Width = GridLength.Auto;
+            }
+            for (int i = 0; i < 14; i++)
+            {
+                Redactor_map.RowDefinitions.Add(new RowDefinition());
+                Redactor_map.RowDefinitions[i].Height = GridLength.Auto;
+                for(int j = 0;j < 24; j++)
+                {
+                    button_SHAB = Set_Button("", (Style)this.Resources["Tiles"]);
+                    button_SHAB.Click += Button_map_Click;
+                    Redactor_map.Children.Add(button_SHAB);
+                    Grid.SetRow(button_SHAB, i);
+                    Grid.SetColumn(button_SHAB, j);
+                }
+            }
         }
         public Button Set_Button(string content, Style st)
         {
             Button button = new Button();
             button.Content = content;
-            button.Name = content;
             button.Style = st;
             return button;
         }
@@ -65,6 +85,14 @@ namespace KR
             button.Background = imageBrush;
             button.Style = st;
             return button;
+        }
+        private void Button_vib_Click(object sender, EventArgs e)
+        {
+            button_SHAB = (Button)sender;
+        }
+        private void Button_map_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Background = button_SHAB.Background;
         }
     }
 }
